e0fe3adbe5287f4b0b7239a1bef5cee3bbbba03c
kouga <907327116@qq.com>
2015-10-27 15:59:10 +0800

    Initial commit

---
+++ MaterialDesignDemo/src/main/java/com/gc/materialdesigndemo/ui/SwitchActivity.java
@@ -0,0 +1,26 @@
+package com.gc.materialdesigndemo.ui;
+
+import com.gc.materialdesigndemo.R;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SwitchActivity extends Activity {
+	
+	int backgroundColor = Color.parseColor("#1E88E5");
+
+    @SuppressLint("NewApi")
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_switchs);
+        int color = getIntent().getIntExtra("BACKGROUND", Color.BLACK);
+        findViewById(R.id.checkBox).setBackgroundColor(color);
+        findViewById(R.id.switchView).setBackgroundColor(color);
+    }  
+    
+
+}
